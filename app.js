e5be2a5f35b802405c3a8e3f3a3ece9dd11e9ee2
raiden357 <raiden357@users.noreply.github.com>
2017-06-17 15:30:16 -0400

    add setOperator, calculate, evaluate, beautify, printResult, and handleClick functions. add event listener to 'calc'.

--- app.js
+++ app.js
@@ -22,4 +22,94 @@ function setNumber(state, newValue) {
 }
 
 // To set an operator
-function set
+function setOperator(state, newValue) {
+	if (newValue == 'c') {
+	  // if operator is clear, reset state
+	  return generateState();
+	}
+	return generateState(newValue, state.numA, state.numB, state.result);
+}
+
+// Generate result
+
+function calculate (state, newValue) {
+	var numA = parseInt(state.numA);
+	var numB= parseInt(state.numB);
+	var operation = state.operation;
+	var result;
+	if (numA == NaN || numB == NaN) {
+	  return generateState(null, null, null, 'Invalid');
+	}
+	switch (operation) {
+	  case '+':
+	    result = numA + numB;
+	    break;
+	  case '-':
+	    result = numA - numB;
+	    break;
+	  case '*':
+	    result = numA * numB;
+	    break;
+	  case '/':
+	    result = numA / numB;
+	    break;
+	  default:
+	  result = null;
+
+	}
+	return generateState(null, result, null, result);
+}
+
+function evaluate(state, newAction, newValue) {
+	switch (newAction) {
+	  case 'number':
+	    return setNumber(state, newValue);
+	  case 'operation':
+	    return setOperator(state, newValue);
+	  case 'calculate':
+	    return calculate(state, newValue);
+	  default: //click elsewhere on the board
+	    return generateState(state.operator, state.numA, state.numB, state.result);
+
+	}
+}
+
+function beautify(num) {
+	var n = num
+	if (n == undefined || n == NaN) {
+	  return 0;
+	}
+	return parseInt(num);
+}
+
+function printResult(state) {
+	elemA.innerHTML = parseInt(state.numA);
+
+	// to avoid showing numB as zero
+
+	var numB = parseInt(state.numb);
+	if (!numB) {
+	  numB = null;
+	}
+	elemB.innerHTML = numB;
+	elemOperator.innerHTML = state.operation || null;
+	elemResult.innerHTML = beautify(state.result);
+
+}
+
+function handleClick(event) {
+	var target = event.target;
+	var action = target.getAttribute('data-action');
+	var value = target.getAttribute('data-value');
+	calculatorState = evaluate(caclulatorState, action, value);
+	printResult(calculatorState);
+}
+
+var calculator = document.getElementById('calc');
+var elemA = document.getElementById('num-a');
+var elemB = document.getElementById('num-b');
+var elemOperator = document.getElementById('operator');
+var elemResult = document.getElementById('result');
+var calculatorState = generateState();
+
+calculator.addEventListener('click', handleClick);
